76cb8a5795b36e4ad02ce1b80b67171c3e2f6626
Nhu Anh <nhuanh@Nhus-MacBook-Air.local>
2022-08-06 11:37:26 +0200

    fix: change to process port for deployment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
 
